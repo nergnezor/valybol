06eaa15c1725a03f4c7f79679be6517e437b6383
nergnezor <e@roseng.ren>
2022-04-15 20:51:30 +0200

    120 hz

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,53 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart' hide Draggable;
 import 'package:flame/game.dart';
 import 'package:flame_rive/flame_rive.dart';
+import 'package:flutter/services.dart';
 import 'package:rive/rive.dart';
 import 'package:flame/components.dart';
 import 'package:flame/input.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
+
+void set120Hz() async {
+  var modes = await FlutterDisplayMode.supported;
+  modes.forEach(print);
+  try {
+    await FlutterDisplayMode.setPreferredMode(modes.last);
+
+    /// e.code =>
+    /// noAPI - No API support. Only Marshmallow and above.
+    /// noActivity -  Activity is not available. Probably app is in background
+  } on PlatformException {
+    return;
+  }
+  MyGame.frameRate = modes.last.refreshRate;
+}
 
 void main() {
   runApp(GameWidget(
     game: MyGame(),
   ));
+
+  try {
+    if (!kIsWeb && Platform.isAndroid) {
+      set120Hz();
+    }
+  } catch (e) {}
 }
 
 class MyGame extends FlameGame with HasTappables, HasDraggables {
+  static double frameRate = 60;
+
   @override
   Color backgroundColor() => const Color(0xff171717);
 
   @override
   Future<void> onLoad() async {
+    BubbleComponent.screenSize = size;
     await super.onLoad();
-    for (double xPosition = 0; xPosition < 800; xPosition += 200) {
-      for (double yPosition = 0; yPosition < 800; yPosition += 200) {
-        await createBubble(xPosition, yPosition);
-      }
-    }
   }
 
   Future<void> createBubble(double xPosition, double yPosition) async {
@@ -48,15 +73,19 @@ class BubbleComponent extends RiveComponent
     with HasGameRef, Tappable, Draggable {
   final Artboard artboard;
   BubbleComponent({required this.artboard})
-      : super(artboard: artboard, size: Vector2.all(200));
+      : super(
+            artboard: artboard,
+            size: Vector2.all(Random().nextDouble() * 100 + 100));
 
   late OneShotAnimation controller;
   late Fill fill;
   Vector2 velocity = Vector2.zero();
+  static late Vector2 screenSize;
 
   @override
   Future<void>? onLoad() {
-    controller = OneShotAnimation('Idle', autoplay: false);
+    
+    controller = OneShotAnimation('Idle', autoplay: true);
     artboard.addController(controller);
     artboard.forEachComponent((child) {
       if (child.name == 'fyllning') {
@@ -69,7 +98,26 @@ class BubbleComponent extends RiveComponent
   @override
   void update(double dt) {
     super.update(dt);
-    position += velocity! * dt * 1000;
+    position += velocity * dt * 10000;
+    velocity *= 0.99;
+    //  bounce
+    if (position.x < 0) {
+      position.x = 0;
+      velocity.x = -velocity.x;
+    }
+    if (position.x > screenSize.x - size.x) {
+      position.x = screenSize.x - size.x;
+      velocity.x = -velocity.x;
+    }
+    if (position.y < 0) {
+      position.y = 0;
+      velocity.y = -velocity.y;
+    }
+    if (position.y > screenSize.y - size.y) {
+      position.y = screenSize.y - size.y;
+      velocity.y = -velocity.y;
+    }
+
   }
 
   @override
