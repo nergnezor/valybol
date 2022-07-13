384869d0d71a2d6fcf44e545e4154a8dc35e41d7
Erik Rosengren <e@roseng.ren>
2022-07-13 08:18:16 +0200

    acc

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,7 @@ class BubbleComponent extends RiveComponent
   double lifeTime = 0;
   double maxVelocity = 0;
   bool growing = true;
-  late AccelerometerEvent acc;
+  AccelerometerEvent? acc;
 
   @override
   Future<void>? onLoad() {
@@ -91,7 +91,15 @@ class BubbleComponent extends RiveComponent
 
   @override
   void update(double dt) {
-    edgeBounce();
+    if (acc != null) {
+      velocity += Vector2(-acc!.x, acc!.y) * dt * 0.1;
+      // position.x = -acc!.x / dt;
+      // print x acc and velocity on same line
+      // print('${acc!.x.toStringAsFixed(2)} ${velocity.x.toStringAsFixed(4)}');
+      // print(acc!.x);
+
+      // print(velocity.x);
+    }
     lifeTime += dt;
     if (lifeTime > 10.0) {
       gameRef.remove(this);
@@ -101,42 +109,50 @@ class BubbleComponent extends RiveComponent
     position += velocity * dt * 10000;
     velocity *= 0.99;
     position.y += dt * 10;
-    size.x = 100-position.y/1000;
-    size.y=size.x;
-    position.x =acc.x/120;
+    size.x = 100 - position.y / 1000;
+    size.y = size.x;
     if (growing) {
       size.x += 2;
       size.y += 2;
       position.x -= 1;
       position.y -= 1;
       if (size.x > 150) {
-        growing=false;
+        growing = false;
         //gameRef.remove(this);
       }
     }
+    edgeBounce();
+    // position.clamp(Vector2.zero(), screenSize - size);
   }
 
   void edgeBounce() {
 //  bounce
     if (position.x < 0 || position.x > screenSize.x - size.x) {
-      velocity.x = -velocity.x;
-      scale.x -= velocity.x.abs() * 2;
+      velocity.x = 0;
+      // -velocity.x;
+      // print(position.x);
+      position.x = max(0, min(screenSize.x - size.x, position.x));
+      // position.x.clamp(100, screenSize.x - size.x);
+      // scale.x -= velocity.x.abs() * 2;
     }
     if (position.y < 0 || position.y > screenSize.y - size.y) {
       velocity.y = -velocity.y;
-      scale.y -= velocity.y.abs() * 2;
+      position.y = max(0, min(screenSize.y - size.y, position.y));
+      // position.y.clamp(0, screenSize.y - size.y);
+      // scale.y -= velocity.y.abs() * 2;
     }
-    position.clamp(Vector2.zero(), screenSize - size);
-    scale.x += (1 - scale.x) * 0.1;
-    scale.y += (1 - scale.y) * 0.1;
-    scale.clamp(Vector2.all(0.5), Vector2.all(1));
+    // position.clamp(Vector2.zero(), screenSize - size);
+    // scale.x += (1 - scale.x) * 0.1;
+    // scale.y += (1 - scale.y) * 0.1;
+    // scale.clamp(Vector2.all(0.5), Vector2.all(1));
   }
 
   @override
   bool onDragStart(DragStartInfo info) {
-    info.handled=true;
+    info.handled = true;
     return false;
   }
+
   @override
   bool onDragEnd(DragEndInfo info) {
     growing = false;
