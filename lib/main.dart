72c85ab5f7d2d58b765d4d55b4ce5e133dad0b8c
Erik Rosengren <e@roseng.ren>
2022-10-26 20:03:14 +0200

    rive_generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
+import 'rive/valybol.rive.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Valybol()));
+  runApp(const MaterialApp(home: Game()));
 }
 
-class Valybol extends StatelessWidget {
-  const Valybol({Key? key}) : super(key: key);
+class Game extends StatelessWidget {
+  const Game({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Center(
       child: RiveAnimation.asset(
-        'assets/valybol.riv',
+        'assets/' + Valybol.assetPath,
         fit: BoxFit.cover,
-        animations: ['waves'],
-        stateMachines: ['State Machine 1'],
+        // animations: ['waves'],
+        // stateMachines: ['State Machine 1'],
       ),
     ));
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:valybol/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MaterialApp(home: Valybol()));
+    await tester.pumpWidget(const MaterialApp(home: Game()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
